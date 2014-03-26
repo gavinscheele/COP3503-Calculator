8e2a7bb5c8c2ff2113f17f4ae8052e4518ff8162
gdscheele <gdscheele@gmail.com>
2014-03-26 18:53:57 -0400

    Delete someFile.h

--- someFile.h
+++
@@ -1,9 +0,0 @@
-        var bindEventToList = function (rootSelector, selector, callback, eventName) {
-            var eName = eventName || 'click';
-            $(rootSelector).on(eName, selector, function () {
-                var project = ko.dataFor(this);
-                callback(project);
-                return false;
-            });
-
-        };
