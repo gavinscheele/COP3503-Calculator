51cd739472a0dc2648284079ef6ff4c59795b143
gdscheele <gdscheele@gmail.com>
2014-03-27 14:35:03 -0400

    Create Expression.cpp
    This .cpp will allow you to us the Expression class, but the canAdd, canSubtract, canMultiply, and canDivide methods cannot be implemented until each subclass is created.

---
+++ Expression.cpp
@@ -0,0 +1,27 @@
+//
+//  Expression.cpp
+//  Calculator
+//
+//  Created by Gavin Scheele on 3/27/14.
+//  Copyright (c) 2014 Gavin Scheele. All rights reserved.
+//
+
+#include "Expression.h"
+Expression::Expression(){
+    
+}
+bool Expression::canAdd(Expression a, Expression b){
+    return false;
+}
+bool Expression::canSubtract(Expression a, Expression b){
+    return false;
+
+}
+bool Expression::canMultiply(Expression a, Expression b){
+    return false;
+
+}
+bool Expression::canDivide(Expression a, Expression b){
+    return false;
+
+}
