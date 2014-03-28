03e167dcc37c161fff27df1935dbd2999eb7f3c2
gdscheele <gdscheele@gmail.com>
2014-03-28 13:50:52 -0400

    Create nthRoot.cpp

---
+++ nthRoot.cpp
@@ -0,0 +1,38 @@
+//
+//  nthRoot.cpp
+//  Calculator
+//
+//  Created by Gavin Scheele on 3/27/14.
+//  Copyright (c) 2014 Gavin Scheele. All rights reserved.
+//
+
+#include "nthRoot.h"
+
+nthRoot::nthRoot(){
+    
+}
+nthRoot::~nthRoot(){
+    
+}
+Expression nthRoot::simplify(){
+    Expression* c = this;
+    return *c;
+}
+
+
+Expression* nthRoot::add(Expression* a, Expression* b){
+    Expression* c = this;
+    return c;
+}
+Expression* nthRoot::subtract(Expression* a, Expression* b){
+    Expression* c = this;
+    return c;
+}
+Expression* nthRoot::multiply(Expression* a, Expression* b){
+    Expression* c = this;
+    return c;
+}
+Expression* nthRoot::divide(Expression* a, Expression* b){
+    Expression* c = this;
+    return c;
+}
