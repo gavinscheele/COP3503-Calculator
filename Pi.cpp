d9431e5dd0ea1db93279a67c53638a6846ec47f5
gdscheele <gdscheele@gmail.com>
2014-03-28 13:52:32 -0400

    Create Pi.cpp

---
+++ Pi.cpp
@@ -0,0 +1,31 @@
+//
+//  Pi.cpp
+//  Calculator
+//
+//  Created by Gavin Scheele on 3/27/14.
+//  Copyright (c) 2014 Gavin Scheele. All rights reserved.
+//
+
+#include "Pi.h"
+Pi::Pi(){
+    
+}
+Pi::~Pi(){
+    
+}
+Expression* Pi::add(Expression* a, Expression* b){
+    Expression* c = this;
+    return c;
+}
+Expression* Pi::subtract(Expression* a, Expression* b){
+    Expression* c = this;
+    return c;
+}
+Expression* Pi::multiply(Expression* a, Expression* b){
+    Expression* c = this;
+    return c;
+}
+Expression* Pi::divide(Expression* a, Expression* b){
+    Expression* c = this;
+    return c;
+}
