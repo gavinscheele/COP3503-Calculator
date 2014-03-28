bdf31e93a46abdb05859c3fc79ec900ad08acc9d
gdscheele <gdscheele@gmail.com>
2014-03-28 13:51:49 -0400

    Create Euler.cpp

---
+++ Euler.cpp
@@ -0,0 +1,31 @@
+//
+//  Euler.cpp
+//  Calculator
+//
+//  Created by Gavin Scheele on 3/27/14.
+//  Copyright (c) 2014 Gavin Scheele. All rights reserved.
+//
+
+#include "Euler.h"
+Euler::Euler(){
+    
+}
+Euler::~Euler(){
+    
+}
+Expression* Euler::add(Expression* a, Expression* b){
+    Expression* c = this;
+    return c;
+}
+Expression* Euler::subtract(Expression* a, Expression* b){
+    Expression* c = this;
+    return c;
+}
+Expression* Euler::multiply(Expression* a, Expression* b){
+    Expression* c = this;
+    return c;
+}
+Expression* Euler::divide(Expression* a, Expression* b){
+    Expression* c = this;
+    return c;
+}
