cc72b40d219a79f9346940f12223ba0f56a34890
gdscheele <gdscheele@gmail.com>
2014-03-31 13:53:47 -0400

    Updated examples

--- Solver.cpp
+++ Solver.cpp
@@ -14,20 +14,38 @@ Solver::~Solver(){
     
 }
 std::string Solver::solve(){
-    //need to find a way to split stirng into expressions and call the simplify methods
+    //need to find a way to split string into expressions and call the simplify methods
     //here is an example of using the Expression methods
     
-    /* Rational a = *new Rational(4, 6);
-     a.simplify();
-     cout << a << endl;
-     
-     Rational b = *new Rational(9,4);
-     b.simplify();
-     cout << b << endl;
-     
-     cout << a.canAdd(&b) << endl;
-     */
+ /* Expression *a = new Pi();
+    Expression *b = new Pi();
+
+    cout << *a << endl;
+    Expression *c = new Rational(a, b);
+    cout << *c << endl;
+    Expression *d = new Rational(9,4);
+    cout << *d << endl;
+    Expression *e = new Euler();
+    cout << *e << endl;
+    Expression *f = new Integer(2);
+    Expression *g = new Integer(3);
+    Expression *h = new Exponential(f,g);
+    cout << *h << endl;
+    Expression *i = new Logarithm(2,3);
+    cout << *i << endl;
+    Expression *j = new nthRoot(3,8);
+    cout << *j << endl;
+    
+    Expression *k = new Euler();
+    Expression *l = new Rational(9,4);
+    Expression *m = new Rational(k,l);
+    cout << *m << endl;  */
+
     
     
-    return localExpression;
+    
+    
+    
+    return "Result: " + localExpression;
 }
+
