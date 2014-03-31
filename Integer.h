e0425ec10bf29524611628d86413fa66b868f576
gdscheele <gdscheele@gmail.com>
2014-03-31 13:46:23 -0400

    Create Integer.h

---
+++ Integer.h
@@ -0,0 +1,28 @@
+//
+//  Integer.h
+//  Calculator
+//
+//  Created by Gavin Scheele on 3/28/14.
+//  Copyright (c) 2014 Gavin Scheele. All rights reserved.
+//
+
+#ifndef __Calculator__Integer__
+#define __Calculator__Integer__
+
+#include <iostream>
+#include "Expression.h"
+class Integer : public Expression{
+public:
+    Integer(int value);
+    ~Integer();
+    
+    Expression* add(Expression* a);
+    Expression* subtract(Expression* a);
+    Expression* multiply(Expression* a);
+    Expression* divide(Expression* a);
+    ostream& print(std::ostream& output) const;
+    
+private:
+    int value;
+};
+#endif /* defined(__Calculator__Integer__) */
