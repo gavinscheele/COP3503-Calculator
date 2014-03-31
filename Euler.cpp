dae63f1fa6e6c8527086abc8a2646df49b9c5366
gdscheele <gdscheele@gmail.com>
2014-03-31 13:41:35 -0400

    Added virtual print function
    Each class specifies how it prints to the console.

--- Euler.cpp
+++ Euler.cpp
@@ -8,24 +8,29 @@
 
 #include "Euler.h"
 Euler::Euler(){
-    
+    this->type = "euler";
+    this->coefficient = 1;
 }
 Euler::~Euler(){
     
 }
-Expression* Euler::add(Expression* a, Expression* b){
+Expression* Euler::add(Expression* a){
     Expression* c = this;
     return c;
 }
-Expression* Euler::subtract(Expression* a, Expression* b){
+Expression* Euler::subtract(Expression* a){
     Expression* c = this;
     return c;
 }
-Expression* Euler::multiply(Expression* a, Expression* b){
+Expression* Euler::multiply(Expression* a){
     Expression* c = this;
     return c;
 }
-Expression* Euler::divide(Expression* a, Expression* b){
+Expression* Euler::divide(Expression* a){
     Expression* c = this;
     return c;
+}
+ostream& Euler::print(std::ostream& output) const{
+    output << this->coefficient << "e";  //overload cout of this class so that it makes sense
+    return output;
 }
