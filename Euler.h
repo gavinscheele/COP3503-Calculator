ce4521d0daa4bfc9cfb1bb07e021e36192dd583e
tessbianchi <tesshbianchi@gmail.com>
2014-04-04 14:20:50 -0400

    Get set methods

--- Euler.h
+++ Euler.h
@@ -21,9 +21,12 @@ public:
     Expression* subtract(Expression* a);
     Expression* multiply(Expression* a);
     Expression* divide(Expression* a);
+	int getCoefficient();
+	void setCoefficient(int x);
     ostream& print(std::ostream& output) const;
 private:
     int coefficient;
 };
 #endif /* defined(__Calculator__Euler__) */
+
 
