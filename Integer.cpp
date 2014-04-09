70939e74abd0d7fe3aa26147157e667626fe8de6
munarriz <munarriz@ufl.edu>
2014-04-08 20:53:11 -0400

    Update Integer.cpp

--- Integer.cpp
+++ Integer.cpp
@@ -12,7 +12,7 @@ Integer::Integer(int value){
     this->value = value;
 }
 Integer::~Integer(){
-    
+
 }
 int Integer::getValue() {
     return value;
@@ -21,23 +21,91 @@ void Integer::setValue(int n) {
     value = n;
 }
 Expression* Integer::add(Expression* a){
-    Expression* c = this;
-    return c;
+    if(a->type == "euler"){
+
+    }else if(a->type == "exponential"){
+
+    }else if(a->type == "integer"){
+
+    }else if(a->type == "logarithm"){
+
+    }else if(a->type == "nthRoot"){
+
+    }else if(a->type == "pi"){
+
+    }else if(a->type == "rational"){
+
+    }else{
+        cout << "type not recognized" << endl;
+    }
+    return this;
 }
 Expression* Integer::subtract(Expression* a){
-    Expression* c = this;
-    return c;
+    if(a->type == "euler"){
+
+    }else if(a->type == "exponential"){
+
+    }else if(a->type == "integer"){
+
+    }else if(a->type == "logarithm"){
+
+    }else if(a->type == "nthRoot"){
+
+    }else if(a->type == "pi"){
+
+    }else if(a->type == "rational"){
+
+    }else{
+        cout << "type not recognized" << endl;
+    }
+    return this;
 }
 Expression* Integer::multiply(Expression* a){
-    Expression* c = this;
-    return c;
+    if(a->type == "euler"){
+
+    }else if(a->type == "exponential"){
+
+    }else if(a->type == "integer"){
+
+    }else if(a->type == "logarithm"){
+
+    }else if(a->type == "nthRoot"){
+
+    }else if(a->type == "pi"){
+
+    }else if(a->type == "rational"){
+
+    }else{
+        cout << "type not recognized" << endl;
+    }
+    return this;
 }
 Expression* Integer::divide(Expression* a){
-    Expression* c = this;
-    return c;
+    if(a->type == "euler"){
+
+    }else if(a->type == "exponential"){
+
+    }else if(a->type == "integer"){
+
+    }else if(a->type == "logarithm"){
+
+    }else if(a->type == "nthRoot"){
+
+    }else if(a->type == "pi"){
+
+    }else if(a->type == "rational"){
+
+    }else{
+        cout << "type not recognized" << endl;
+    }
+    return this;
 }
 ostream& Integer::print(std::ostream& output) const{
     output << this->value;
     return output;
 }
 
+string Integer::toString() {
+    string str = value;
+    return str;
+}
