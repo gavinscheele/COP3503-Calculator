b11a5667274fb66861fc2df236e6c5709cae48a2
munarriz <munarriz@ufl.edu>
2014-04-08 20:50:50 -0400

    Update Exponential.cpp

--- Exponential.cpp
+++ Exponential.cpp
@@ -8,60 +8,147 @@
 
 #include "Exponential.h"
 
-Exponential::Exponential(Expression* base, Expression* exponent){
+
+Exponential::Exponential(Expression* base, Rational* exponent){
     this->type = "exponential";
     this->base = base;
     this->exponent = exponent;
+    Integer *exde = (Integer *)exponent->getDenominator();
+    if (exde->getValue() != 1) {
+        base = nthRoot(exde, base);
+        exponent = exponent->getNumerator();
+    }
+    Integer *exnu = (Integer *)exponent->getNumerator();
+    if (canExponentiate()) {
+    this = exponentiate();
+    }
 }
 Exponential::~Exponential(){
-    
+
 }
 
-Expression Exponential::exponentiate(){
-    if (this->exponent>0) {
-        return exponentiate(this->base, this->exponent);
-    }
-    if (this->exponent==0) {
-        return 1;
-    }
-    if (this->exponent<0) {
-        return (1/exponentiate(this->base, this->exponent));
-    }
+bool Exponential::canExponentiate() {
+    // will return true if expression can be multiplied out
+    return true;
 }
 
-Expression Exponential:exponentiate(Expression expression, int exponent) {
-    if (n>1) {
-    return exponentiate(this*expression, n-1);
+Expression* Exponential::exponentiate(){
+    Expression* toReturn = base;
+    Expression* constantBase = base;
+    if (this->exponent.getNumerator()==0) {
+        Integer ret = new Integer(1);
+        return ret;
     }
-    return expression;
+    bool toFlip = false;
+    if (exnu<0) {
+            toFlip = true;
+    }
+    Expression* e = this;
+    for (int i = 1; i < exnu); i++) {
+        toReturn*=constantBase;
+    }
+    exnu = 1;
+    if (toFlip()) {
+        return 1/toReturn;
+    }
+    return toReturn;
+
 }
 
 Expression* Exponential::add(Expression* a){
-    Expression* c = this;
-    return c;
+    if(a->type == "euler"){
+
+    }else if(a->type == "exponential"){
+
+    }else if(a->type == "integer"){
+
+    }else if(a->type == "logarithm"){
+
+    }else if(a->type == "nthRoot"){
+
+    }else if(a->type == "pi"){
+
+    }else if(a->type == "rational"){
+
+    }else{
+        cout << "type not recognized" << endl;
+    }
+    return this;
 }
 Expression* Exponential::subtract(Expression* a){
-    Expression* c = this;
-    return c;
+    if(a->type == "euler"){
+
+    }else if(a->type == "exponential"){
+
+    }else if(a->type == "integer"){
+
+    }else if(a->type == "logarithm"){
+
+    }else if(a->type == "nthRoot"){
+
+    }else if(a->type == "pi"){
+
+    }else if(a->type == "rational"){
+
+    }else{
+        cout << "type not recognized" << endl;
+    }
+    return this;
 }
 Expression* Exponential::multiply(Expression* a){
-    Expression* c = this;
+    /*Expression* c = this;
     if (*a == *c) //might require overriding
     {
         exponent += a->getExponent;
     }
-    return c;
+    return c;*/
+    if(a->type == "euler"){
+
+    }else if(a->type == "exponential"){
+
+    }else if(a->type == "integer"){
+
+    }else if(a->type == "logarithm"){
+
+    }else if(a->type == "nthRoot"){
+
+    }else if(a->type == "pi"){
+
+    }else if(a->type == "rational"){
+
+    }else{
+        cout << "type not recognized" << endl;
+    }
+    return this;
 }
 Expression* Exponential::divide(Expression* a){
-    Expression* c = this;
+    /*Expression* c = this;
     if (*a == *c) //might require overriding
     {
         c->getExponent -= a->getExponent;
     }
-    return c;
+    return c;*/
+    if(a->type == "euler"){
+
+    }else if(a->type == "exponential"){
+
+    }else if(a->type == "integer"){
+
+    }else if(a->type == "logarithm"){
+
+    }else if(a->type == "nthRoot"){
+
+    }else if(a->type == "pi"){
+
+    }else if(a->type == "rational"){
+
+    }else{
+        cout << "type not recognized" << endl;
+    }
+    return this;
 }
 
-*Expression Exponential::getExponent() {
+Rational Exponential::getExponent() {
     return exponent;
 }
 
@@ -69,12 +156,17 @@ Expression* Exponential::divide(Expression* a){
     return base;
 }
 
-void Exponential::setExponent(*Expression e) {
+void Exponential::setExponent(Rational e) {
     exponent = e;
 }
 
 void Exponential::setBase(*Expression e) {
     base = e;
+}
+
+string Exponential::toString() {
+    string str = base + "^" + exponent;
+    return str;
 }
 
 
