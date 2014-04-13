04a142fe07be486ee76713d42769343c6c9261b1
Danny Groves <dgroves1522@yahoo.com>
2014-04-13 15:52:38 -0400

    Update nthRoot.cpp

--- nthRoot.cpp
+++ nthRoot.cpp
@@ -44,7 +44,7 @@ Expression* nthRoot::simplify(){
     //if coefficient == 0 then return 0?
     //if operand < 0 throw an error
     if ((root % 2) == 0 && operand < 0) {   //this needs to be made right
-        cout<<"Error: Can't take the root of a negative number"; //needs to say, for example, Source: Sqrt:-1
+        cout<<"Error: Can't take the root of a negative number \n Source" << this->toString(); //needs to say, for example, Source: Sqrt:-1
         return this;
     }
     factors = this->primeFactorization();
