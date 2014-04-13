7fb712cb9f673226354aadc47b94050645e666ea
Danny Groves <dgroves1522@yahoo.com>
2014-04-13 17:24:59 -0400

    got started

--- main.cpp
+++ main.cpp
@@ -8,9 +8,27 @@
 
 #include "Solver.h"
 using namespace std;
-int main(int argc, char * argv[])
+int main(int argc, char * argv[])  //a main method shouldn't have perameters - at least I don't think so. -Dan
 {
-
+    string choice;
+    choice = "z";
+    cout << "Menu" << "\n" <<
+           "a. Compute a New Expression" << "\n" <<
+           "b. Help" << "\n" <<
+           "c. Review Previous Expressions and Answers" << "\n";
+    cin >> choice;
+    cout << "\n";
+    if (choice.compare("a") == 0) {
+        string secondChoice = "n"
+        while (secondChoice.compare("y") == 0) {
+            string expression;
+            cin >> expression;
+            //that's the input - I don't know how you want to handle it
+            cout <<"\n" << "Would you like to go back to the menu? (y/n)" << "\n";
+            cin >> secondChoice;
+            cout << "\n";
+        }
+    }
 //    Rational *a = new Rational(2,1);
 //    Rational *b = new Rational(1,-8);
 //    Rational *c = new Rational(2,1);
