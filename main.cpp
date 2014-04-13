43c67cce54028a35b8f420eab2bbbb15060ec5f7
Danny Groves <dgroves1522@yahoo.com>
2014-04-13 17:39:48 -0400

    the structure is there

--- main.cpp
+++ main.cpp
@@ -8,27 +8,43 @@
 
 #include "Solver.h"
 using namespace std;
-int main(int argc, char * argv[])  //a main method shouldn't have perameters - at least I don't think so. -Dan
-{
+int main(int argc, char * argv[])  {//a main method shouldn't have perameters - at least I don't think so. -Dan
     string choice;
     choice = "z";
-    cout << "Menu" << "\n" <<
+    while (choice.compare("d") != 0) {
+        cout << "Menu" << "\n" <<
            "a. Compute a New Expression" << "\n" <<
            "b. Help" << "\n" <<
-           "c. Review Previous Expressions and Answers" << "\n";
-    cin >> choice;
-    cout << "\n";
-    if (choice.compare("a") == 0) {
-        string secondChoice = "n"
-        while (secondChoice.compare("y") == 0) {
-            string expression;
-            cin >> expression;
-            //that's the input - I don't know how you want to handle it
-            cout <<"\n" << "Would you like to go back to the menu? (y/n)" << "\n";
-            cin >> secondChoice;
-            cout << "\n";
+           "c. Review Previous Expressions and Answers" << "\n" <<
+           "d. Quit" << "\n";
+        cin >> choice;
+        cout << "\n";
+        if (choice.compare("a") == 0) {
+            string secondChoice = "n"
+            while (secondChoice.compare("y") != 0) {
+                string expression;
+                cin >> expression;
+                //that's the input - I don't know how you want to handle it
+                cout <<"\n" << "Would you like to go back to the menu? (y/n)" << "\n";
+                cin >> secondChoice;
+                cout << "\n";
+            }
+        }
+        if (choice.compare("b") == 0) {
+            cout << "Directions for Use of This Program" << "\n";
+        }
+        if (choice.compare("c") == 0) {
+            //we're screwed
+        }
+        else {
+            cout << "Sorry, couldn't recognize your input." << "\n";        
         }
     }
+    cout << "Thank You" << "\n";
+    return 0;
+        
+}
+/*
 //    Rational *a = new Rational(2,1);
 //    Rational *b = new Rational(1,-8);
 //    Rational *c = new Rational(2,1);
@@ -47,3 +63,4 @@ int main(int argc, char * argv[])  //a main method shouldn't have perameters - a
     
     return 0;
 }
+*/
